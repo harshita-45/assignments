eb5d5d0e7c2e476c24f314e855fa9baba9c59138
harshita-45 <harshitasinghsrmcem@gmail.com>
2025-08-14 00:01:12 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+Python 3.13.6 (tags/v3.13.6:4e66535, Aug  6 2025, 14:36:00) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> for i in range(2, 6):
+...     for j in range(1, i + 1):
+...         print(j, end="")
+...     print()
+... 
+...     
+12
+123
+1234
+12345

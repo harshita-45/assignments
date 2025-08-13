4e7c8d885806a404f9b87e82fc7cf9b519ad1584
harshita-45 <harshitasinghsrmcem@gmail.com>
2025-08-13 23:53:19 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+Python 3.13.6 (tags/v3.13.6:4e66535, Aug  6 2025, 14:36:00) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a = 5
+>>> b = 10
+>>> print("before swapping:")
+before swapping:
+>>> print("a =", a)
+a = 5
+>>> print("b =", b)
+b = 10
+>>> a, b = b, a
+>>> print("after swapping:")
+after swapping:
+>>> print("a =",a)
+a = 10
+>>> print("b =", b)
+b = 5
